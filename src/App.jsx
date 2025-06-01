7ae9223203e5edbcd6324a1569f9c8d240ca64b6
ANCHAL <anchal2004tandekar@gmail.com>
2025-06-01 17:30:06 +0530

    added rigid body to card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 import { Model } from "./components/Model";
-
+import { Band } from "./components/Band";
+import { Physics } from "@react-three/rapier";
 function App(){
     return(
         <Canvas camera={{position: [0, 0, 10], fov : 75}}>
             <ambientLight intensity={1} />
-            <pointLight position={[10, 10, 10]} intensity={10} />
-            <Model />
+            <pointLight position={[10, 10, 10]} intensity={1} />
+            <Physics>
+                <Model />
+                <Band />
+            </Physics>
             <OrbitControls />
         </Canvas>
     )
--- src/components/Band.jsx
+++ src/components/Band.jsx
@@ -4,15 +4,48 @@ import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
 import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier'
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 
-function Band(){
+extend({ MeshLineGeometry, MeshLineMaterial })
+
+export function Band() {
     const band = useRef()
     const fixed = useRef()
     const j1 = useRef()
     const j2 = useRef()
     const j3 = useRef()
 
-    const {width, height} = useThree((state) => state.size)
+    const { width, height } = useThree((state) => state.size)
     const [curve] = useState(() => new THREE.CatmullRomCurve3([
         new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()
     ]))
+
+    useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1])
+    useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1])
+    useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1])
+
+    useFrame(() => {
+        curve.points[0].copy(j3.current.translation())
+        curve.points[1].copy(j2.current.translation())
+        curve.points[2].copy(j1.current.translation())
+        curve.points[3].copy(fixed.current.translation())
+        band.current.geometry.setPoints(curve.getPoints(32))
+    })
+
+    return (
+        <>
+            <RigidBody ref={fixed} type="fixed" />
+            <RigidBody position={[0.5, 0, 0]} ref={j1}>
+                <BallCollider args={[0.1]} />
+            </RigidBody>
+            <RigidBody position={[1, 0, 0]} ref={j2}>
+                <BallCollider args={[0.1]} />
+            </RigidBody >
+            <RigidBody position={[1.5, 0, 0]} ref={j3}>
+                <BallCollider args={[0.1]} />
+            </RigidBody >
+            <mesh ref={band}>
+                <meshLineGeometry />
+                <meshLineMaterial color="white" resolution={[width, height]} lineWidth={1} />
+            </mesh>
+        </>
+    )
 }
