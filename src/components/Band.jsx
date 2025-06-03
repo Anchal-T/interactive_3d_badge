7ead783413b36292fc0c56595fb24c7dba0a7b59
ANCHAL <anchal2004tandekar@gmail.com>
2025-06-03 22:59:01 +0530

    formated the code

--- src/components/Band.jsx
+++ src/components/Band.jsx
@@ -1,112 +1,197 @@
-import * as THREE from 'three'
-import { useEffect, useRef, useState } from 'react'
-import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
-import { useGLTF, useTexture, Environment, Lightformer } from '@react-three/drei'
-import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier'
-import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
-import { useControls } from 'leva'
+import * as THREE from "three";
+import { useEffect, useRef, useState } from "react";
+import { Canvas, extend, useThree, useFrame } from "@react-three/fiber";
+import {
+  useGLTF,
+  useTexture,
+  Environment,
+  Lightformer,
+} from "@react-three/drei";
+import {
+  BallCollider,
+  CuboidCollider,
+  Physics,
+  RigidBody,
+  useRopeJoint,
+  useSphericalJoint,
+} from "@react-three/rapier";
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import { useControls } from "leva";
 
-extend({ MeshLineGeometry, MeshLineMaterial })
-useGLTF.preload('/model/keyCard.glb')
-useTexture.preload('/textures/pikachuL.png')
+extend({ MeshLineGeometry, MeshLineMaterial });
+useGLTF.preload("/model/keyCard.glb");
+useTexture.preload("/textures/pikachuL.png");
 
 export function Band({ maxSpeed = 50, minSpeed = 10 }) {
-    const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef() // prettier-ignore
-    const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3() // prettier-ignore
-    const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 2, linearDamping: 2 }
-    const { nodes, materials } = useGLTF('/model/keyCard.glb')
-    const texture = useTexture('/textures/pikachuL.png')
-    const { width, height } = useThree((state) => state.size)
-    const [curve] = useState(() => new THREE.CatmullRomCurve3([new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()]))
-    const [dragged, drag] = useState(false)
-    const [hovered, hover] = useState(false)
+  const band = useRef(),
+    fixed = useRef(),
+    j1 = useRef(),
+    j2 = useRef(),
+    j3 = useRef(),
+    card = useRef();
+  const vec = new THREE.Vector3(),
+    ang = new THREE.Vector3(),
+    rot = new THREE.Vector3(),
+    dir = new THREE.Vector3();
+  const segmentProps = {
+    type: "dynamic",
+    canSleep: true,
+    colliders: false,
+    angularDamping: 2,
+    linearDamping: 2,
+  };
+  const { nodes, materials } = useGLTF("/model/keyCard.glb");
+  const texture = useTexture("/textures/pikachuL.png");
+  const { width, height } = useThree((state) => state.size);
+  const [curve] = useState(
+    () =>
+      new THREE.CatmullRomCurve3([
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+      ])
+  );
+  const [dragged, drag] = useState(false);
+  const [hovered, hover] = useState(false);
 
-    useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
-    useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
-    useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore  
-    useSphericalJoint(j3, card, [[0, 0, 0], [0, 1.45, 0]])
+  useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1]);
+  useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1]);
+  useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1]);
+  useSphericalJoint(j3, card, [
+    [0, 0, 0],
+    [0, 1.45, 0],
+  ]);
 
-    useEffect(() => {
-        if (hovered) {
-            document.body.style.cursor = dragged ? 'grabbing' : 'grab'
-            return () => void (document.body.style.cursor = 'auto')
-        }
-    }, [hovered, dragged])
+  useEffect(() => {
+    if (hovered) {
+      document.body.style.cursor = dragged ? "grabbing" : "grab";
+      return () => void (document.body.style.cursor = "auto");
+    }
+  }, [hovered, dragged]);
 
-    useFrame((state, delta) => {
-        if (dragged) {
-            vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera)
-            dir.copy(vec).sub(state.camera.position).normalize()
-            vec.add(dir.multiplyScalar(state.camera.position.length()))
-                ;[card, j1, j2, j3, fixed].forEach((ref) => ref.current?.wakeUp())
-            card.current?.setNextKinematicTranslation({ x: vec.x - dragged.x, y: vec.y - dragged.y, z: vec.z - dragged.z })
-        }
-        if (fixed.current) {
-            // Fix most of the jitter when over pulling the card
-            ;[j1, j2].forEach((ref) => {
-                if (!ref.current.lerped) ref.current.lerped = new THREE.Vector3().copy(ref.current.translation())
-                const clampedDistance = Math.max(0.1, Math.min(1, ref.current.lerped.distanceTo(ref.current.translation())))
-                ref.current.lerped.lerp(ref.current.translation(), delta * (minSpeed + clampedDistance * (maxSpeed - minSpeed)))
-            })
-            // Calculate catmul curve
-            curve.points[0].copy(j3.current.translation())
-            curve.points[1].copy(j2.current.lerped)
-            curve.points[2].copy(j1.current.lerped)
-            curve.points[3].copy(fixed.current.translation())
-            band.current.geometry.setPoints(curve.getPoints(32))
-            // Tilt it back towards the screen
-            ang.copy(card.current.angvel())
-            rot.copy(card.current.rotation())
-            card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z })
-        }
-    })
+  useFrame((state, delta) => {
+    if (dragged) {
+      vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera);
+      dir.copy(vec).sub(state.camera.position).normalize();
+      vec.add(dir.multiplyScalar(state.camera.position.length()));
+      [card, j1, j2, j3, fixed].forEach((ref) => ref.current?.wakeUp());
+      card.current?.setNextKinematicTranslation({
+        x: vec.x - dragged.x,
+        y: vec.y - dragged.y,
+        z: vec.z - dragged.z,
+      });
+    }
+    if (fixed.current) {
+      [j1, j2].forEach((ref) => {
+        if (!ref.current.lerped)
+          ref.current.lerped = new THREE.Vector3().copy(
+            ref.current.translation()
+          );
+        const clampedDistance = Math.max(
+          0.1,
+          Math.min(1, ref.current.lerped.distanceTo(ref.current.translation()))
+        );
+        ref.current.lerped.lerp(
+          ref.current.translation(),
+          delta * (minSpeed + clampedDistance * (maxSpeed - minSpeed))
+        );
+      });
+      curve.points[0].copy(j3.current.translation());
+      curve.points[1].copy(j2.current.lerped);
+      curve.points[2].copy(j1.current.lerped);
+      curve.points[3].copy(fixed.current.translation());
+      band.current.geometry.setPoints(curve.getPoints(32));
 
-    curve.curveType = 'chordal'
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+      ang.copy(card.current.angvel());
+      rot.copy(card.current.rotation());
+      card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z });
+    }
+  });
 
-    return (
-        <>
-            <group position={[0, 4, 0]}>
-                <RigidBody ref={fixed} {...segmentProps} type="fixed" />
-                <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}>
-                    <BallCollider args={[0.1]} />
-                </RigidBody>
-                <RigidBody position={[1, 0, 0]} ref={j2} {...segmentProps}>
-                    <BallCollider args={[0.1]} />
-                </RigidBody>
-                <RigidBody position={[1.5, 0, 0]} ref={j3} {...segmentProps}>
-                    <BallCollider args={[0.1]} />
-                </RigidBody>
-                <RigidBody position={[2, 0, 0]} ref={card} {...segmentProps} type={dragged ? 'kinematicPosition' : 'dynamic'}>
-                    <CuboidCollider args={[0.8, 1.125, 0.01]} />
-                    <group
-                        scale={2.25}
-                        position={[0, -1.2, -0.05]}
-                        onPointerOver={() => hover(true)}
-                        onPointerOut={() => hover(false)}
-                        onPointerUp={(e) => (e.target.releasePointerCapture(e.pointerId), drag(false))}
-                        onPointerDown={(e) => (e.target.setPointerCapture(e.pointerId), drag(new THREE.Vector3().copy(e.point).sub(vec.copy(card.current.translation()))))}>                        <mesh geometry={nodes.Plane.geometry} position={[-0.002, 0.674, -0.011]} rotation={[-2.405, 1.566, -2.309]} scale={0.357}>
-                            <meshPhysicalMaterial 
-                                map={texture} 
-                                map-rotation={Math.PI / 2 + Math.PI / 2 
-                                    + Math.PI / 2
-                                }
-                                map-anisotropy={16} 
-                                clearcoat={1} 
-                                clearcoatRoughness={0.15} 
-                                roughness={0.3} 
-                                metalness={0.5} 
-                            />
-                        </mesh>
-                        <mesh geometry={nodes.clamp.geometry} material={materials.metal} position={[0.001, 1.067, -0.038]} rotation={[-0.086, -0.113, 0.1]} scale={1.285} />
-                        <mesh geometry={nodes.clip.geometry} material={materials.metal} position={[-0.01, 1.203, -0.049]} rotation={[-0.206, 1.033, 0.211]} scale={0.991} />
-                    </group>
-                </RigidBody>
-            </group>
-            <mesh ref={band}>
-                <meshLineGeometry />
-                <meshLineMaterial color="black" depthTest={false} resolution={[width, height]} lineWidth={1} />
+  curve.curveType = "chordal";
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+  return (
+    <>
+      <group position={[0, 4, 0]}>
+        <RigidBody ref={fixed} {...segmentProps} type="fixed" />
+        <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody position={[1, 0, 0]} ref={j2} {...segmentProps}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody position={[1.5, 0, 0]} ref={j3} {...segmentProps}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody
+          position={[2, 0, 0]}
+          ref={card}
+          {...segmentProps}
+          type={dragged ? "kinematicPosition" : "dynamic"}
+        >
+          <CuboidCollider args={[0.8, 1.125, 0.01]} />
+          <group
+            scale={2.25}
+            position={[0, -1.2, -0.05]}
+            onPointerOver={() => hover(true)}
+            onPointerOut={() => hover(false)}
+            onPointerUp={(e) => (
+              e.target.releasePointerCapture(e.pointerId), drag(false)
+            )}
+            onPointerDown={(e) => (
+              e.target.setPointerCapture(e.pointerId),
+              drag(
+                new THREE.Vector3()
+                  .copy(e.point)
+                  .sub(vec.copy(card.current.translation()))
+              )
+            )}
+          >
+            {" "}
+            <mesh
+              geometry={nodes.Plane.geometry}
+              position={[-0.002, 0.674, -0.011]}
+              rotation={[-2.405, 1.566, -2.309]}
+              scale={0.357}
+            >
+              <meshPhysicalMaterial
+                map={texture}
+                map-rotation={Math.PI / 2 + Math.PI / 2 + Math.PI / 2}
+                map-anisotropy={16}
+                clearcoat={1}
+                clearcoatRoughness={0.15}
+                roughness={0.3}
+                metalness={0.5}
+              />
             </mesh>
-        </>
-    )
+            <mesh
+              geometry={nodes.clamp.geometry}
+              material={materials.metal}
+              position={[0.001, 1.067, -0.038]}
+              rotation={[-0.086, -0.113, 0.1]}
+              scale={1.285}
+            />
+            <mesh
+              geometry={nodes.clip.geometry}
+              material={materials.metal}
+              position={[-0.01, 1.203, -0.049]}
+              rotation={[-0.206, 1.033, 0.211]}
+              scale={0.991}
+            />
+          </group>
+        </RigidBody>
+      </group>
+      <mesh ref={band}>
+        <meshLineGeometry />
+        <meshLineMaterial
+          color="black"
+          depthTest={false}
+          resolution={[width, height]}
+          lineWidth={1}
+        />
+      </mesh>
+    </>
+  );
 }
