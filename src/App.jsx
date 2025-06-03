a5c35022ed6bd9aaeb787533872556047ff17ff0
ANCHAL <anchal2004tandekar@gmail.com>
2025-06-03 20:50:20 +0530

    removed debug

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import { useControls } from 'leva'
 
 function App(){
     const cardModelRef = useRef();
-    const { debug } = useControls({ debug: true })
+    // const { debug } = useControls({ debug: true })
 
     return(
         <Canvas camera={{position: [0, 0, 13], fov : 15}}>
             <ambientLight intensity={Math.PI} />
             <pointLight position={[10, 10, 10]} intensity={4.5} />
-            <Physics debug={debug} interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
+            <Physics interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
                 <Band cardRef={cardModelRef} />
                 <Model ref={cardModelRef} position={[0, 0, 0]}/> 
             </Physics>
